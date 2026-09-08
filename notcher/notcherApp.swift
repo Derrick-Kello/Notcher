93205c6596e16727475c57ff81dd47f98fb8cddd
Derrick Jnr Kello <derrickkello48@gmail.com>
2026-09-08 02:07:45 +0000

    Initial Commit

---
+++ notcher/notcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  notcherApp.swift
+//  notcher
+//
+//  Created by Developer Goals on 9/8/26.
+//
+
+import SwiftUI
+
+@main
+struct notcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
